db79e960620c515ed502a09c04f0c7b714944c92
Angel Guevara <angeldelcaos@gmail.com>
2015-07-25 03:09:10 -0500

    Fixed targetAttributes for exist rule generator

--- generators/model/Generator.php
+++ generators/model/Generator.php
@@ -322,8 +322,12 @@ class Generator extends \yii\gii\Generator
             $refClassName = $this->generateClassName($refs[0]);
             unset($refs[0]);
             $attributes = implode("', '", array_keys($refs));
-            $targetAttributes = implode("', '", array_values($refs));
-            $rules[] = "[['$attributes'], 'exist', 'skipOnError' => true, 'targetClass' => $refClassName::className(), 'targetAttribute' => ['$targetAttributes']]";
+            $targetAttributes = '';
+            foreach ($refs as $key => $value) {
+                $targetAttributes .= "'$key' => '$value', ";
+            }
+            $targetAttributes = trim($targetAttributes);
+            $rules[] = "[['$attributes'], 'exist', 'skipOnError' => true, 'targetClass' => $refClassName::className(), 'targetAttribute' => [$targetAttributes]]";
         }
 
         return $rules;
