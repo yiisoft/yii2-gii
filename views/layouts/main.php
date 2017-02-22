253a3355290bc65efe46b8fe0bdcf43de43c8e4a
Dmitriy Bashkarev <dmitriy@bashkarev.com>
2017-02-23 02:18:10 +0300

    Fixes #252: Added meta tag to prevent indexing of debug by search engines in case it's exposed

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,6 +14,7 @@ $asset = yii\gii\GiiAsset::register($this);
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="none">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
