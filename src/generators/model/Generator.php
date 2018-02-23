67743f6a2d352ab51fd362ea8199b3cbe2c8598e
Robert Korulczyk <robert@korulczyk.pl>
2018-02-23 12:04:39 +0100

    Require Yii 2.0.14 (#339)

--- src/generators/model/Generator.php
+++ src/generators/model/Generator.php
@@ -322,7 +322,7 @@ class Generator extends \yii\gii\Generator
                     $types['boolean'][] = $column->name;
                     break;
                 case Schema::TYPE_FLOAT:
-                case 'double': // Schema::TYPE_DOUBLE, which is available since Yii 2.0.3
+                case Schema::TYPE_DOUBLE:
                 case Schema::TYPE_DECIMAL:
                 case Schema::TYPE_MONEY:
                     $types['number'][] = $column->name;
