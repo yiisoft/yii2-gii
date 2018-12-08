55c365390567112bb5ba252eaf04c2969c50ed1f
Alexander Makarov <sam@rmcreative.ru>
2018-12-08 12:41:11 +0300

    Added missing phpdoc block [skip ci]

--- src/generators/extension/default/AutoloadExample.php
+++ src/generators/extension/default/AutoloadExample.php
@@ -1,4 +1,9 @@
-<?= "<?php\n" ?>
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
 
 namespace <?= substr($generator->namespace, 0, -1) ?>;
 
