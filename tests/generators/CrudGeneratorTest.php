e3ca333e8b7a1d2f189c5c73715c12c3a62c8a42
Tobias Munk <schmunk@usrbin.de>
2019-02-14 12:01:37 +0100

    added more controller generation tests

--- tests/generators/CrudGeneratorTest.php
+++ tests/generators/CrudGeneratorTest.php
@@ -53,6 +53,18 @@ class CrudGeneratorTest extends TestCase
         $g = new Generator();
         $g->controllerClass = '\app\controllers\ATestController';
         $this->assertEquals('a-test', $g->getControllerID());
+
+        $g = new Generator();
+        $g->controllerClass = '\app\controllers\YoTestController';
+        $this->assertEquals('yo-test', $g->getControllerID());
+
+        $g = new Generator();
+        $g->controllerClass = '\app\controllers\ABCTestController';
+        $this->assertEquals('a-b-c-test', $g->getControllerID());
+
+        $g = new Generator();
+        $g->controllerClass = '\app\controllers\XYTestController';
+        $this->assertEquals('x-y-test', $g->getControllerID());
     }
 
 }
