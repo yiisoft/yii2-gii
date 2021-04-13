622761f492f46fb0a18bd026bcc32c243e642ce8
Anton <info@ensostudio.ru>
2021-04-13 10:00:00 +0300

    Fix #439: Replace client-side generation of model class name with an AJAX request and a serverside implementation to take options into account

--- src/generators/model/form.php
+++ src/generators/model/form.php
@@ -1,19 +1,26 @@
 <?php
 
 use yii\gii\generators\model\Generator;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
 /* @var $generator yii\gii\generators\model\Generator */
 
-echo $form->field($generator, 'tableName')->textInput(['table_prefix' => $generator->getTablePrefix()]);
-echo $form->field($generator, 'modelClass');
-echo $form->field($generator, 'standardizeCapitals')->checkbox();
-echo $form->field($generator, 'singularize')->checkbox();
-echo $form->field($generator, 'ns');
-echo $form->field($generator, 'baseClass');
 echo $form->field($generator, 'db');
 echo $form->field($generator, 'useTablePrefix')->checkbox();
+echo $form->field($generator, 'useSchemaName')->checkbox();
+echo $form->field($generator, 'tableName')->textInput([
+    'data' => [
+        'table-prefix' => $generator->getTablePrefix(), 
+        'action' => Url::to(['default/action', 'id' => 'model', 'name' => 'GenerateClassName'])
+    ]
+]);
+echo $form->field($generator, 'standardizeCapitals')->checkbox();
+echo $form->field($generator, 'singularize')->checkbox();
+echo $form->field($generator, 'modelClass');
+echo $form->field($generator, 'ns');
+echo $form->field($generator, 'baseClass');
 echo $form->field($generator, 'generateRelations')->dropDownList([
     Generator::RELATIONS_NONE => 'No relations',
     Generator::RELATIONS_ALL => 'All relations',
@@ -31,4 +38,3 @@ echo $form->field($generator, 'queryClass');
 echo $form->field($generator, 'queryBaseClass');
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
-echo $form->field($generator, 'useSchemaName')->checkbox();
