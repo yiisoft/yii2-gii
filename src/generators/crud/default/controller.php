f2d76c452a79bcbc4e1e889574cc2ce0e810564d
Anton <info@ensostudio.ru>
2021-09-25 14:39:33 +0300

    Update view `generators/crud/default/controller`
    Fix return type in PhpDoc
    Add Php 7 features (optional)
    Use `Yii::createObject()` instead `new className()`

--- src/generators/crud/default/controller.php
+++ src/generators/crud/default/controller.php
@@ -1,125 +1,135 @@
 <?php
 /**
  * This is the template for generating a CRUD controller class file.
+ *
+ * @var \yii\web\View $this
+ * @var \yii\gii\generators\crud\Generator $generator
  */
 
 use yii\db\ActiveRecordInterface;
 use yii\helpers\StringHelper;
 
-
-/* @var $this yii\web\View */
-/* @var $generator yii\gii\generators\crud\Generator */
-
 $controllerClass = StringHelper::basename($generator->controllerClass);
 $modelClass = StringHelper::basename($generator->modelClass);
-$searchModelClass = StringHelper::basename($generator->searchModelClass);
-if ($modelClass === $searchModelClass) {
-    $searchModelAlias = $searchModelClass . 'Search';
+if (!empty($generator->searchModelClass)) {
+    $searchModelClass = StringHelper::basename($generator->searchModelClass);
+    if ($modelClass === $searchModelClass) {
+        $searchModelAlias = $searchModelClass . 'Search';
+    }
 }
 
-/* @var $class ActiveRecordInterface */
-$class = $generator->modelClass;
+/** @var $class ActiveRecord */
+$class = ltrim($generator->modelClass, '\\');
 $pks = $class::primaryKey();
 $urlParams = $generator->generateUrlParams();
 $actionParams = $generator->generateActionParams();
 $actionParamComments = $generator->generateActionParamComments();
+$php7 = PHP_MAJOR_VERSION === 7;
 
 echo "<?php\n";
 ?>
 
 namespace <?= StringHelper::dirname(ltrim($generator->controllerClass, '\\')) ?>;
 
-use <?= ltrim($generator->modelClass, '\\') ?>;
+use <?= $class ?>;
 <?php if (!empty($generator->searchModelClass)): ?>
-use <?= ltrim($generator->searchModelClass, '\\') . (isset($searchModelAlias) ? " as $searchModelAlias" : "") ?>;
+use <?= ltrim($generator->searchModelClass, '\\') . (isset($searchModelAlias) ? " as $searchModelAlias" : '') ?>;
 <?php else: ?>
 use yii\data\ActiveDataProvider;
 <?php endif; ?>
 use <?= ltrim($generator->baseControllerClass, '\\') ?>;
+use yii\web\Response;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use Yii;
 
 /**
- * <?= $controllerClass ?> implements the CRUD actions for <?= $modelClass ?> model.
+ * Controller implements the CRUD actions for `<?= $modelClass ?>` model.
  */
 class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->baseControllerClass) . "\n" ?>
 {
     /**
      * @inheritDoc
      */
-    public function behaviors()
+    public function behaviors()<?= ($php7 ? ': array' : '') . "\n" ?>
     {
         return array_merge(
             parent::behaviors(),
             [
-                'verbs' => [
-                    'class' => VerbFilter::className(),
+                'verbFilter' => [
+                    'class' => VerbFilter::class<?= $php7 ? '' : 'Name()' ?>,
                     'actions' => [
+                        'index' => ['GET'],
+                        'view' => ['GET'],
+                        'create' => ['GET', 'POST'],
+                        'update' => ['GET', 'POST'],
                         'delete' => ['POST'],
-                    ],
-                ],
+                    ]
+                ]
             ]
         );
     }
 
     /**
      * Lists all <?= $modelClass ?> models.
-     * @return mixed
+     * @return string
      */
-    public function actionIndex()
+    public function actionIndex()<?= ($php7 ? ': string' : '') . "\n" ?>
     {
 <?php if (!empty($generator->searchModelClass)): ?>
-        $searchModel = new <?= isset($searchModelAlias) ? $searchModelAlias : $searchModelClass ?>();
+        $searchModel = Yii::createObject(<?= isset($searchModelAlias) ? $searchModelAlias : $searchModelClass ?>::class<?= $php7 ? '' : 'Name()' ?>);
         $dataProvider = $searchModel->search($this->request->queryParams);
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+        return $this->render(
+            'index',
+            [
+                'searchModel' => $searchModel,
+                'dataProvider' => $dataProvider,
+            ]
+        );
 <?php else: ?>
-        $dataProvider = new ActiveDataProvider([
-            'query' => <?= $modelClass ?>::find(),
-            /*
-            'pagination' => [
-                'pageSize' => 50
-            ],
-            'sort' => [
-                'defaultOrder' => [
+        $dataProvider = Yii::createObject(
+            ActiveDataProvider::class<?= $php7 ? '' : 'Name()' ?>,
+            [
+                'query' => <?= $modelClass ?>::find(),
+                /*
+                'pagination' => [
+                    'pageSize' => 50
+                ],
+                'sort' => [
+                    'defaultOrder' => [
 <?php foreach ($pks as $pk): ?>
-                    <?= "'$pk' => SORT_DESC,\n" ?>
+                        <?= "'$pk' => SORT_DESC,\n" ?>
 <?php endforeach; ?>
+                    ]
                 ]
-            ],
-            */
-        ]);
+                */
+            ]
+        );
 
-        return $this->render('index', [
-            'dataProvider' => $dataProvider,
-        ]);
+        return $this->render('index', ['dataProvider' => $dataProvider]);
 <?php endif; ?>
     }
 
     /**
      * Displays a single <?= $modelClass ?> model.
      * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return mixed
+     * @return string
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionView(<?= $actionParams ?>)
+    public function actionView(<?= $actionParams ?>)<?= ($php7 ? ': string' : '') . "\n" ?>
     {
-        return $this->render('view', [
-            'model' => $this->findModel(<?= $actionParams ?>),
-        ]);
+        return $this->render('view', ['model' => $this->findModel(<?= $actionParams ?>)]);
     }
 
     /**
      * Creates a new <?= $modelClass ?> model.
      * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
+     * @return Response|string
      */
     public function actionCreate()
     {
-        $model = new <?= $modelClass ?>();
+        $model = Yii::createObject(<?= $modelClass ?>);
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
@@ -129,16 +139,14 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
             $model->loadDefaultValues();
         }
 
-        return $this->render('create', [
-            'model' => $model,
-        ]);
+        return $this->render('create', ['model' => $model]);
     }
 
     /**
      * Updates an existing <?= $modelClass ?> model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return mixed
+     * @return Response|string
      * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionUpdate(<?= $actionParams ?>)
@@ -149,19 +157,17 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
             return $this->redirect(['view', <?= $urlParams ?>]);
         }
 
-        return $this->render('update', [
-            'model' => $model,
-        ]);
+        return $this->render('update', ['model' => $model]);
     }
 
     /**
      * Deletes an existing <?= $modelClass ?> model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return mixed
+     * @return Response
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionDelete(<?= $actionParams ?>)
+    public function actionDelete(<?= $actionParams ?>): Response
     {
         $this->findModel(<?= $actionParams ?>)->delete();
 
@@ -175,7 +181,7 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
      * @return <?=                   $modelClass ?> the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel(<?= $actionParams ?>)
+    protected function findModel(<?= $actionParams ?>): <?= $modelClass ?>
     {
 <?php
 $condition = [];
@@ -184,10 +190,11 @@ foreach ($pks as $pk) {
 }
 $condition = '[' . implode(', ', $condition) . ']';
 ?>
-        if (($model = <?= $modelClass ?>::findOne(<?= $condition ?>)) !== null) {
-            return $model;
+        $model = <?= $modelClass ?>::findOne(<?= $condition ?>);
+        if ($model === null) {
+            throw new NotFoundHttpException(<?= $generator->generateString('The requested page does not exist.') ?>);
         }
 
-        throw new NotFoundHttpException(<?= $generator->generateString('The requested page does not exist.') ?>);
+        return $model;
     }
 }
