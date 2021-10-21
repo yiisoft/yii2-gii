c32fa8c4f6b5f6492696eb5dc83403f37a45325c
Anton <info@ensostudio.ru>
2021-10-21 17:07:50 +0300

    Update Generator.php

--- src/generators/model/Generator.php
+++ src/generators/model/Generator.php
@@ -415,7 +415,7 @@ class Generator extends \yii\gii\Generator
                 case Schema::TYPE_DATE:
                 case Schema::TYPE_TIME:
                 case Schema::TYPE_DATETIME:
-              case Schema::TYPE_TIMESTAMP:
+                case Schema::TYPE_TIMESTAMP:
                 case Schema::TYPE_JSON:
                     $types['safe'][] = $column->name;
                     break;
