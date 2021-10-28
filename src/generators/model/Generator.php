876098c76500df7b193a2629747bee6fa936a1e3
Anton <info@ensostudio.ru>
2021-10-28 16:46:23 +0300

    Minor code cleanup for model generator (#479)
    1. `requiredTemplates()` checks `$queryClass`
    2. getDbConnection() always returns `Connection`, see `validateDb()`
    3. update methods using `getDbConnection()`.
    4. formats code of `rules()` to readable

--- src/generators/model/Generator.php
+++ src/generators/model/Generator.php
@@ -36,8 +36,17 @@ class Generator extends \yii\gii\Generator
 
     public $db = 'db';
     public $ns = 'app\models';
+    /**
+     * @var string
+     */
     public $tableName;
+    /**
+     * @var string
+     */
     public $modelClass;
+    /**
+     * @var string
+     */
     public $baseClass = 'yii\db\ActiveRecord';
     public $generateRelations = self::RELATIONS_ALL;
     public $generateJunctionRelationMode = self::JUNCTION_RELATION_VIA_TABLE;
@@ -49,9 +58,23 @@ class Generator extends \yii\gii\Generator
     public $useSchemaName = true;
     public $generateQuery = false;
     public $queryNs = 'app\models';
+    /**
+     * @var string
+     */
     public $queryClass;
+    /**
+     * @var string
+     */
     public $queryBaseClass = 'yii\db\ActiveQuery';
 
+    /**
+     * @var string[]|null
+     */
+    protected $tableNames;
+    /**
+     * @var string[]
+     */
+    protected $classNames = [];
 
     /**
      * @inheritdoc
@@ -75,8 +98,14 @@ class Generator extends \yii\gii\Generator
     public function rules()
     {
         return array_merge(parent::rules(), [
-            [['db', 'ns', 'tableName', 'modelClass', 'baseClass', 'queryNs', 'queryClass', 'queryBaseClass'], 'filter', 'filter' => 'trim'],
-            [['ns', 'queryNs'], 'filter', 'filter' => function ($value) { return trim($value, '\\'); }],
+            [['db', 'tableName', 'modelClass', 'baseClass', 'queryClass', 'queryBaseClass'], 'filter', 'filter' => 'trim'],
+            [
+                ['ns', 'queryNs'],
+                'filter',
+                'filter' => static function ($value) {
+                    return $value === null ? null : trim($value, ' \\');
+                }
+            ],
             [['db', 'ns', 'tableName', 'baseClass', 'queryNs', 'queryBaseClass'], 'required'],
             [['db', 'modelClass', 'queryClass'], 'match', 'pattern' => '/^\w+$/', 'message' => 'Only word characters are allowed.'],
             [['ns', 'baseClass', 'queryNs', 'queryBaseClass'], 'match', 'pattern' => '/^[\w\\\\]+$/', 'message' => 'Only word characters and backslashes are allowed.'],
@@ -89,14 +118,16 @@ class Generator extends \yii\gii\Generator
             [['queryBaseClass'], 'validateClass', 'params' => ['extends' => ActiveQuery::className()]],
             [['generateRelations'], 'in', 'range' => [self::RELATIONS_NONE, self::RELATIONS_ALL, self::RELATIONS_ALL_INVERSE]],
             [['generateJunctionRelationMode'], 'in', 'range' => [self::JUNCTION_RELATION_VIA_TABLE, self::JUNCTION_RELATION_VIA_MODEL]],
-            [['generateLabelsFromComments', 'useTablePrefix', 'useSchemaName', 'generateQuery', 'generateRelationsFromCurrentSchema'], 'boolean'],
-            [['enableI18N', 'standardizeCapitals', 'singularize'], 'boolean'],
+            [
+                ['generateLabelsFromComments', 'useTablePrefix', 'useSchemaName', 'generateQuery', 'generateRelationsFromCurrentSchema', 'enableI18N', 'standardizeCapitals', 'singularize'],
+                'boolean'
+            ],
             [['messageCategory'], 'validateMessageCategory', 'skipOnEmpty' => false],
         ]);
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function attributeLabels()
     {
@@ -121,7 +152,7 @@ class Generator extends \yii\gii\Generator
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function hints()
     {
@@ -191,11 +222,16 @@ class Generator extends \yii\gii\Generator
      */
     public function requiredTemplates()
     {
-        return ['model.php'];
+        $templates = ['model.php'];
+        if ($this->queryClass !== null) {
+            $templates[] = 'query.php';
+        }
+
+        return $templates;
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function stickyAttributes()
     {
@@ -239,9 +275,9 @@ class Generator extends \yii\gii\Generator
         $relations = $this->generateRelations();
         $db = $this->getDbConnection();
         foreach ($this->getTableNames() as $tableName) {
-            // model :
+            // model:
             $modelClassName = $this->generateClassName($tableName);
-            $queryClassName = ($this->generateQuery) ? $this->generateQueryClassName($modelClassName) : false;
+            $queryClassName = $this->generateQuery ? $this->generateQueryClassName($modelClassName) : false;
             $tableRelations = isset($relations[$tableName]) ? $relations[$tableName] : [];
             $tableSchema = $db->getTableSchema($tableName);
             $params = [
@@ -260,7 +296,7 @@ class Generator extends \yii\gii\Generator
                 $this->render('model.php', $params)
             );
 
-            // query :
+            // query:
             if ($queryClassName) {
                 $params['className'] = $queryClassName;
                 $params['modelClassName'] = $modelClassName;
@@ -740,7 +776,7 @@ class Generator extends \yii\gii\Generator
             // ignore
         }
         foreach ($uniqueKeys as $uniqueKey) {
-            if (count(array_diff(array_merge($uniqueKey, $fks), array_intersect($uniqueKey, $fks))) === 0) {
+            if (array_diff(array_merge($uniqueKey, $fks), array_intersect($uniqueKey, $fks)) === []) {
                 return false;
             }
         }
@@ -935,9 +971,6 @@ class Generator extends \yii\gii\Generator
         }
     }
 
-    protected $tableNames;
-    protected $classNames;
-
     /**
      * @return array the table names that match the pattern specified by [[tableName]].
      */
@@ -1076,7 +1109,7 @@ class Generator extends \yii\gii\Generator
     /**
      * Returns the database connection as specified by [[db]].
      *
-     * @return Connection|object|null
+     * @return Connection|null database connection instance
      */
     protected function getDbConnection()
     {
@@ -1084,13 +1117,15 @@ class Generator extends \yii\gii\Generator
     }
 
     /**
+     * Returns the driver name of [[db]] connection.
+     *
      * @return string|null driver name of db connection.
-     * In case [[db]] is not instance of \yii\db\Connection null will be returned.
      * @since 2.0.6
      */
     protected function getDbDriverName()
     {
         $db = $this->getDbConnection();
+
         return $db instanceof Connection ? $db->driverName : null;
     }
 
