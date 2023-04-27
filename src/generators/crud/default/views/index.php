5dc2db2350960295132ce32016c6453eb2d87c66
Ori Grata <42106367+origrata@users.noreply.github.com>
2023-04-27 20:36:38 +0700

    Update index.php

--- src/generators/crud/default/views/index.php
+++ src/generators/crud/default/views/index.php
@@ -41,6 +41,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php if ($generator->indexWidgetType === 'grid'): ?>
     <?= "<?= " ?>GridView::widget([
         'dataProvider' => $dataProvider,
+        'pager' => ['class' => 'yii\bootstrap5\LinkPager'],
         <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n        'columns' => [\n" : "'columns' => [\n"; ?>
             ['class' => 'yii\grid\SerialColumn'],
 
