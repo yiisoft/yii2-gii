yiisoft/yii2-gii

commit 3e270e1cc46dbfe164b503ab8511a59ddc363459

Fix #551: Add optional parameter `$formName` to `SearchModel::search()` method

src/generators/crud/default/search.php
@@ -57,10 +57,11 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
      * Creates data provider instance with search query applied
      *
      * @param array $params
+     * @param string $formName Form name to be used into `->load()` method. By default it is equal to the class name.
      *
      * @return ActiveDataProvider
      */
-    public function search($params)
+    public function search($params, $formName = '<?= $searchModelClass ?>')
     {
         $query = <?= isset($modelAlias) ? $modelAlias : $modelClass ?>::find();
 
@@ -70,7 +71,7 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
             'query' => $query,
         ]);
 
-        $this->load($params);
+        $this->load($params, $formName);
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails

commit 79aceaf616e54cc447ee51eb82ba70697180559a

Fix and issue introduced in PR #552 (#553)
Co-authored-by: Robert Korulczyk <robert@korulczyk.pl>
Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

src/generators/crud/default/search.php
@@ -57,11 +57,11 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
      * Creates data provider instance with search query applied
      *
      * @param array $params
-     * @param string $formName Form name to be used into `->load()` method. By default it is equal to the class name.
+     * @param string|null $formName Form name to be used into `->load()` method.
      *
      * @return ActiveDataProvider
      */
-    public function search($params, $formName = '<?= $searchModelClass ?>')
+    public function search($params, $formName = null)
     {
         $query = <?= isset($modelAlias) ? $modelAlias : $modelClass ?>::find();
 