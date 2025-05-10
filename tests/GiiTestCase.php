a7b6e587120f627079fb947bcda652f7faff1f70
Wilmer Arambula <terabytesoftw@gmail.com>
2025-05-10 18:45:52 -0400

    Use Yii2 `2.2`.

--- tests/GiiTestCase.php
+++ tests/GiiTestCase.php
@@ -13,7 +13,7 @@ class GiiTestCase extends TestCase
 {
     protected $driverName = 'sqlite';
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
